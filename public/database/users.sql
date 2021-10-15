6c6f17b1c89bb58971178fc0abee48758f454f03
Kat Buermann <56609591+TheGreatFoxxy@users.noreply.github.com>
2021-10-15 14:34:59 -0500

    We do not register users here

--- public/database/users.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE users;
-CREATE TABLE user (
-  id  INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  email     VARCHAR(255)    NOT NULL,
-  create_date   DATETIME    NOT NULL,
-  password      VARCHAR(255)   NOT NULL,
-  last_name     VARCHAR(255),
-  first_name    VARCHAR(255),
-);
